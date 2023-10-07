2e13a2551a06947856ba8bb6b0f72a6d20167674
diamantove <diamantove@mail.ru>
2023-10-07 12:02:17 +0300

    Добавлено 3 языка

---
+++ hello.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var eng := ReadInteger('Введите язык (1 - русский, 2 - английский, 3 - арабский):');
+  assert((eng >= 1) and (eng <= 3));
+  
+  var s := Lst('Привет!', 'Hello!', 'مرحبًا!');
+  print(s[eng - 1])
+end.
